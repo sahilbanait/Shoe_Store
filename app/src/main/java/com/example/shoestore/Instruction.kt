0280fab28068c6cfcfcb37bd37d79e0e6762a3d4
Sahil Banait <sahil.saini526@gmail.com>
2021-08-01 22:25:12 +0800

    Intial Commit

---
+++ app/src/main/java/com/example/shoestore/Instruction.kt
@@ -0,0 +1,21 @@
+package com.example.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Instruction : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.instruction, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/shoestore/Welcome.kt
@@ -0,0 +1,21 @@
+package com.example.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Welcome : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.welcome, container, false)
+    }
+
+
+}

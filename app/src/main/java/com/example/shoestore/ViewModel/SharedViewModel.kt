f2372af31fddf8ba386c203f248f60a6f747dc67
sahilbanait <77091616+sahilbanait@users.noreply.github.com>
2021-08-30 11:53:50 +0800

    Changes to ViewModel

--- app/src/main/java/com/example/shoestore/ViewModel/SharedViewModel.kt
+++ app/src/main/java/com/example/shoestore/ViewModel/SharedViewModel.kt
@@ -8,6 +8,7 @@ import com.example.shoestore.data.Shoe_List_Data
 class SharedViewModel(): ViewModel(){
 
     private lateinit var shoeList: MutableList<Shoe_List_Data>
+    private var shoes = mutableListOf<Shoe_List_Data>()
 
     private val _shoeLiveData = MutableLiveData<List<Shoe_List_Data>>()
     val dataList:LiveData<List<Shoe_List_Data>>
@@ -22,12 +23,15 @@ class SharedViewModel(): ViewModel(){
     }
     fun onSave(shoe_name: String, shoe_company: String, shoe_size:String, shoe_description: String){
         val new_Shoe = Shoe_List_Data(shoe_name,shoe_company,shoe_size,shoe_description)
-        val addShoe  = shoeList.toMutableList()
+        /*val addShoe  = shoeList.toMutableList()
         shoeList.clear()
         addShoe.add(new_Shoe)
         shoeList=addShoe
-        _shoeLiveData.value=shoeList
-
+        _shoeLiveData.value=shoeList*/
+        new_Shoe?.let { shoe ->
+            shoes.add(shoe)
+            _shoeLiveData.value = shoes
+        }
     }
 
     override fun onCleared() {
